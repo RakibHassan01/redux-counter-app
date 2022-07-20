242ac8c72be7120c41b863a0711a297b9e987d22
unknown <rhbappy456@gmail.com>
2022-07-20 22:11:52 +0600

    counter updated

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
     const handleIncrement = () => {
-      setCount(count + 1)
+    //   setCount(count + 1)
     }
     return (
         <div>
             <h2>Redux Counter APP</h2>
-            <h3>Count : {count}</h3>
+            {/* <h3>Count : {count}</h3> */}
+            <h3>Count : 0</h3>
             <button onClick={handleIncrement}>Increment</button>
         </div>
     );
 };
 
 export default Counter;
+
+//Counter1 -> Counter2 -> Counter3 -> Counter4
+
+// 01.state - count - 0
+// 02.actions - increment, decrement, reset
+// 03.reducer - increment -> count + 1 
+//         - decrement -> count - 1
+//         - reset -> count = 0
+
+//04.store -
